02801fdd1908ec43903f302c999498dcd8e00c6c
1TopG <ipz232_pov@student.ztu.edu.ua>
2025-05-25 22:24:14 +0300

    [FEATURE] Implemented controller and security to routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,11 @@ use routes\ShipmentStrategies\DeleteShipmentStrategy;
 use routes\ShipmentStrategies\GetShipmentCollectingStrategy;
 use routes\ShipmentStrategies\GetShipmentStrategy;
 use routes\ShipmentStrategies\UpdateShipmentStrategy;
+use routes\TrackingStatusStrategies\AddTrackingStatusStrategy;
+use routes\TrackingStatusStrategies\DeleteTrackingStatusStrategy;
+use routes\TrackingStatusStrategies\GetTrackingStatusCollectionStrategy;
+use routes\TrackingStatusStrategies\GetTrackingStatusStrategy;
+use routes\TrackingStatusStrategies\UpdateTrackingStatusStrategy;
 use routes\UserStrategies\AddUserStrategy;
 use routes\UserStrategies\DeleteUserStrategy;
 use routes\UserStrategies\GetUserCollectionStrategy;
@@ -16,6 +21,10 @@ use security\ShipmentSecurity\DeleteShipmentSecurity;
 use security\ShipmentSecurity\GetShipmentCollectionSecurity;
 use security\ShipmentSecurity\GetShipmentSecurity;
 use security\ShipmentSecurity\UpdateShipmentSecurity;
+use security\TrackingStatusSecurity\DeleteTrackingStatusSecurity;
+use security\TrackingStatusSecurity\GetTrackingStatusCollectionSecurity;
+use security\TrackingStatusSecurity\GetTrackingStatusSecurity;
+use security\TrackingStatusSecurity\UpdateTrackingStatusSecurity;
 use security\UserSecurity\DeleteUserSecurity;
 use security\UserSecurity\GetUserCollectionSecurity;
 use security\UserSecurity\GetUserSecurity;
@@ -39,6 +48,12 @@ $router->register('GET', '#^/shipments/(\d+)$#', new GetShipmentSecurity(new Get
 $router->register('POST', '#^/shipments#', new AddShipmentStrategy());
 $router->register('PATCH', '#^/shipments/(\d+)$#', new UpdateShipmentSecurity(new UpdateShipmentStrategy()));
 $router->register('DELETE', '#^/shipments/(\d+)$#', new DeleteShipmentSecurity(new DeleteShipmentStrategy()));
+
+$router->register('GET', '#^/tracking-status$#', new GetTrackingStatusCollectionSecurity(new GetTrackingStatusCollectionStrategy()));
+$router->register('GET', '#^/tracking-status/(\d+)$#', new GetTrackingStatusSecurity(new GetTrackingStatusStrategy()));
+$router->register('POST', '#^/tracking-status#', new AddTrackingStatusStrategy());
+$router->register('PATCH', '#^/tracking-status/(\d+)$#', new UpdateTrackingStatusSecurity(new UpdateTrackingStatusStrategy()));
+$router->register('DELETE', '#^/tracking-status/(\d+)$#', new DeleteTrackingStatusSecurity(new DeleteTrackingStatusStrategy()));
 
 $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
 $method = $_SERVER['REQUEST_METHOD'];
